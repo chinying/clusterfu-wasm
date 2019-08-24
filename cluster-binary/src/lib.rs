4d4f32fbbd0d94c4fd4496be8f89bf29e66f58e9
Seah Chin Ying <scy.6char@gmail.com>
2019-08-25 00:35:05 +0800

    Expose cluster function to wasm

--- cluster-binary/src/lib.rs
+++ cluster-binary/src/lib.rs
@@ -9,6 +9,12 @@ macro_rules! log {
 }
 
 use wasm_bindgen::prelude::*;
+mod fuzzy_distance_cluster;
+mod fuzzy_point_map;
+use std::vec::Vec;
+use std::collections::{HashMap, BinaryHeap};
+use crate::fuzzy_point_map::FuzzyPointMap;
+use crate::fuzzy_distance_cluster::FuzzyDistanceCluster;
 
 #[wasm_bindgen]
 extern {
@@ -25,7 +31,25 @@ pub fn add(int: u32) -> u32 {
   return (2 + int) as u32;
 }
 
-// #[wasm_bindgen]
-// pub fn cluster(float: f32) {
+#[wasm_bindgen]
+pub fn cluster(x_arr: Vec<f64>, y_arr: Vec<f64>, w_arr: Vec<f64>) -> String {
+  let g_size = 15.0;
+  let distance = 100.0;
 
-// }
+  // log!("x: {:?}, y: {:?}, w: {:?}", x_arr, y_arr, w_arr);
+  let v1 = x_arr.iter()
+    .zip(y_arr.iter())
+    .zip(w_arr.iter())
+    .map(|((x, y), w)| {
+      fuzzy_point_map::FuzzyPoint{x: *x, y: *y, weight: *w}
+    })
+    .collect::<Vec<fuzzy_point_map::FuzzyPoint>>();
+
+  log!("{:?}", v1);
+
+  let indexed_vec = v1.iter().enumerate()
+    .map(|(index, &fzm)| (fzm, index as i32))
+    .collect::<Vec<(fuzzy_point_map::FuzzyPoint, i32)>>();
+  let fuzzy_cluster_app = fuzzy_distance_cluster::FuzzyDistanceClusterClass{distance: distance, grid_size: distance * 0.09};
+  format!("{:?}", fuzzy_cluster_app.apply(indexed_vec)).into()
+}
