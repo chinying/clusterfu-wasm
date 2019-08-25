fad9b53132023fe79b173421e4e88e350a081e88
Seah Chin Ying <scy.6char@gmail.com>
2019-08-25 21:53:07 +0800

    Add vue2 google maps dependency

---
+++ src/components/ClusterMap.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div id="map">
+      <GmapMap
+        :center="{lat:1.3521, lng:103.8198}"
+        :zoom="12"
+        map-type-id="roadmap"
+        style="width: 100%; height: 100vh;"
+        :options="{styles: mapConfig}"
+        id="gmap"
+        ref="mapRef"
+      ></GmapMap>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+const VueGoogleMaps = require('vue2-google-maps')
+const mapConfig = require('./mapStyles.json')
+
+export default Vue.extend({
+  name: 'ClusterMap',
+  components: {
+    GmapMap: VueGoogleMaps.Map,
+    GmapMarker: VueGoogleMaps.Marker,
+    GmapCircle: VueGoogleMaps.Circle,
+  },
+  data () {
+    return {
+      mapConfig
+    }
+  }
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+const VueGoogleMaps = require('vue2-google-maps')
 
 Vue.config.productionTip = false;
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY
+  }
+})
 
 new Vue({
   router,
