1374283ade774673dcde808ec176c9a4e2198da8
Seah Chin Ying <scy.6char@gmail.com>
2019-08-29 11:20:11 +0800

    Push cluster response to map

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { ClusterResponse } from './types/cluster'
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    dataWithCoordinates: [] as Array<Array<string>>
+    dataWithCoordinates: [] as Array<Array<string>>,
+    clusters: [] as Array<ClusterResponse>
   },
   mutations: {
     setDataWithCoordinates (state, val: Array<Array<string>>) {
       state.dataWithCoordinates = val
+    },
+    setClusters (state, val: Array<ClusterResponse>)  {
+      state.clusters = val
     }
   },
   actions: {}
---
+++ src/types/cluster.ts
@@ -0,0 +1,5 @@
+export interface ClusterResponse {
+  x: number,
+  y: number,
+  weight: number
+};
